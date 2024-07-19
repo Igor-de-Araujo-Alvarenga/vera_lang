cc6155a37a53aee1847c059f0094c7e075922922
igor <igor.alvarenga@compuletra.com.br>
2024-07-18 22:51:41 -0300

    remove useless functions and add expressions with identifiers

--- build/vera.c
+++ build/vera.c
@@ -1,13 +1,18 @@
 #include <stdio.h> 
 
 int main(){ 
-char* mensagem = "deu certo";
-if(1 != 1)
- { 
-printf("%s", mensagem); 
-}else{ 
-printf("%s", "deu errado"); 
-} 
+int num1 = 100;
+int num2 = 230;
+int resultado = (100 + 230);
+printf("%s", "\nNumero 1: "); 
+printf("%d", num1); 
+printf("%s", "\n"); 
+printf("%s", "Numero 2: "); 
+printf("%d", num2); 
+printf("%s", "\n"); 
+printf("%s", "Resultado: "); 
+printf("%d", resultado); 
+printf("%s", "\n"); 
 return 0; 
 
  }
--- src/parser.rs
+++ src/parser.rs
@@ -1,17 +1,19 @@
-use std::fmt::format;
 use crate::tokenizer::Token;
 use crate::tokenizer::Token::Identifier;
 use std::collections::HashMap;
-pub struct Parser{
+use std::fmt::format;
+use std::thread::panicking;
+pub struct Parser {
     tokens: Vec<Token>,
     current: usize,
     pub symbol_table: HashMap<String, Token>,
+    pub value_table: HashMap<String, ASTNode>,
 }
 #[derive(Debug, Clone)]
 pub enum ASTNode {
-    Identifier{
+    Identifier {
         name: String,
-        data_type: Token
+        data_type: Token,
     },
     Declaration {
         data_type: Token,
@@ -23,28 +25,31 @@ pub enum ASTNode {
     BinaryOp {
         left: Box<ASTNode>,
         op: Token,
-        right: Box<ASTNode>
+        right: Box<ASTNode>,
     },
     Main {
-        body: Vec<ASTNode>
+        body: Vec<ASTNode>,
     },
     Print(Token),
     If {
-        block: Vec<ASTNode>
+        block: Vec<ASTNode>,
     },
-    ElseIf{
-        block: Vec<ASTNode>
+    ElseIf {
+        block: Vec<ASTNode>,
     },
-    Else{
-        block: Vec<ASTNode>
-    }
+    Else {
+        block: Vec<ASTNode>,
+    },
 }
 
-impl Parser
-{
-    pub fn new(tokens: Vec<Token>) -> Self
-    {
-        Parser { tokens, current: 0, symbol_table: HashMap::new() }
+impl Parser {
+    pub fn new(tokens: Vec<Token>) -> Self {
+        Parser {
+            tokens,
+            current: 0,
+            symbol_table: HashMap::new(),
+            value_table: HashMap::new()
+        }
     }
 
     fn parse_and_push<F>(&mut self, body: &mut Vec<ASTNode>, token: Token, parse_fn: F)
@@ -131,8 +136,7 @@ impl Parser
         Some(ASTNode::Else { block: else_ast })
     }
 
-    fn parse_main2(&mut self) -> ASTNode
-    {
+    fn parse_main2(&mut self) -> ASTNode {
         self.consume(&Token::LParen);
         self.consume(&Token::RParen);
         self.consume(&Token::LBrace);
@@ -143,40 +147,38 @@ impl Parser
                     if let Some(print_stmt) = self.parse_print() {
                         body.push(print_stmt);
                     }
-                },
+                }
                 Token::IntegerType | Token::StringType | Token::BooleanType => {
                     if let Some(declaration) = self.parse_declaration() {
                         body.push(declaration);
                     }
-                },
+                }
                 Token::If => {
-                    if let Some(if_stmt) = self.parse_if_condition(){
+                    if let Some(if_stmt) = self.parse_if_condition() {
                         body.push(if_stmt);
                     }
                 }
                 Token::ElseIf => {
-                    if let Some(else_if_stmt) = self.parse_else_if_condition(){
+                    if let Some(else_if_stmt) = self.parse_else_if_condition() {
                         body.push(else_if_stmt);
                     }
                 }
                 Token::Else => {
-                    if let Some(else_stmt ) = self.parse_else_condition(){
+                    if let Some(else_stmt) = self.parse_else_condition() {
                         body.push(else_stmt);
                     }
                 }
-                _ => { panic!("Unsupported feature: {:?}.", self.peek())}
+                _ => {
+                    panic!("Unsupported feature: {:?}.", self.peek())
+                }
             }
         }
         self.consume(&Token::RBrace);
-        ASTNode::Main {
-            body: body,
-        }
+        ASTNode::Main { body: body }
     }
-    fn parse_expression(&mut self) -> Option<ASTNode>
-    {
+    fn parse_expression(&mut self) -> Option<ASTNode> {
         let mut node = self.parse_term();
-        while self.match_token(&[Token::Plus, Token::Minus])
-        {
+        while self.match_token(&[Token::Plus, Token::Minus]) {
             let op = self.previous().clone();
             let right = self.parse_term();
             node = ASTNode::BinaryOp {
@@ -187,79 +189,81 @@ impl Parser
         }
         Some(node)
     }
-    fn parse_logic_expression(&mut self) -> Option<ASTNode>
-    {
-        let mut node = self.parse_logic_factor();
-        while self.match_token(&[Token::LessThan, Token::LessEqualThan, Token::BiggerEqualThan,
-        Token::BiggerThan, Token::DifferentThan, Token::EqualThan])
-        {
-            let op = self.previous().clone();
-            let right = self.parse_logic_factor();
-            node = ASTNode::BinaryOp {
-                left : Box::new(node),
-                op,
-                right: Box::new(right)
-            }
-        }
-        Some(node)
-    }
-    fn parse_logic_factor(&mut self) -> ASTNode
-    {
-        if self.match_token(&[Token::LParen])
-        {
-            let logic_expr = self.parse_logic_expression();
-            self.consume(&Token::RParen);
-            logic_expr.unwrap()
-        }
-        else if let Token::Identifier(ident) = self.peek()
-        {
-            let ident_clone = ident.clone();
-            let data_type = self.symbol_table.get(&ident_clone).unwrap();
-            let ident = ASTNode::Identifier{
-                name: ident_clone.clone(),
-                data_type: data_type.clone()
-            };
-            self.consume(&Token::Identifier(ident_clone.to_string()));
-            ident
-        }
-        else if let Token::Number(num) = self.peek()
-        {
-            let num_string = num.to_string();
-            self.consume(&Token::Number(num_string.parse().unwrap()));
-            ASTNode::Number(num_string.parse().unwrap())
-        }
-        else {
-            panic!("Expected logic expression {:?}", self.tokens[self.current])
-        }
-    }
-    fn parse_term(&mut self) -> ASTNode
-    {
+
+    fn parse_term(&mut self) -> ASTNode {
         let mut node = self.parse_factor();
-        while self.match_token(&[Token::Multiply, Token::Divide])
-        {
+        while self.match_token(&[Token::Multiply, Token::Divide]) {
             let op = self.previous().clone();
             let right = self.parse_factor();
             node = ASTNode::BinaryOp {
                 left: Box::new(node),
                 op,
-                right: Box::new(right)
+                right: Box::new(right),
             };
         }
         node
     }
 
-    fn parse_factor(&mut self) -> ASTNode
-    {
+    fn parse_factor(&mut self) -> ASTNode {
         if self.match_token(&[Token::LParen]) {
-            let expr = self.parse_expression();
+            let expr = self.parse_expression().unwrap();
             self.consume(&Token::RParen);
-            expr.unwrap()
-        } else if let Token::Number(num) = self.advance() {
-            ASTNode::Number(num.parse().unwrap())
+            return expr;
+        } else if let Some(Token::Number(num)) = self.match_number() {
+            return ASTNode::Number(num.parse().unwrap());
+        } else if let Some(Token::Identifier(ident)) = self.match_identifier() {
+            if let Some(value) = self.value_table.get(&ident) {
+                return value.clone();
+            } else {
+                panic!("Unknown identifier: {}", ident);
+            }
         } else {
-            panic!("Expected expression")
+            panic!("Expected expression but found {:?}", self.peek());
         }
     }
+    fn parse_logic_expression(&mut self) -> Option<ASTNode> {
+        let mut node = self.parse_logic_factor();
+        while self.match_token(&[
+            Token::LessThan,
+            Token::LessEqualThan,
+            Token::BiggerEqualThan,
+            Token::BiggerThan,
+            Token::DifferentThan,
+            Token::EqualThan,
+        ]) {
+            let op = self.previous().clone();
+            let right = self.parse_logic_factor();
+            node = ASTNode::BinaryOp {
+                left: Box::new(node),
+                op,
+                right: Box::new(right),
+            }
+        }
+        Some(node)
+    }
+    fn parse_logic_factor(&mut self) -> ASTNode {
+        if self.match_token(&[Token::LParen]) {
+            let logic_expr = self.parse_logic_expression();
+            self.consume(&Token::RParen);
+            logic_expr.unwrap()
+        } else if let Token::Identifier(ident) = self.peek() {
+            let ident_clone = ident.clone();
+            let data_type = self.symbol_table.get(&ident_clone).unwrap();
+            let ident = ASTNode::Identifier {
+                name: ident_clone.clone(),
+                data_type: data_type.clone(),
+            };
+            self.consume(&Token::Identifier(ident_clone.to_string()));
+            ident
+        } else if let Token::Number(num) = self.peek() {
+            let num_string = num.to_string();
+            self.consume(&Token::Number(num_string.parse().unwrap()));
+            ASTNode::Number(num_string.parse().unwrap())
+        } else {
+            panic!("Expected logic expression {:?}", self.tokens[self.current])
+        }
+    }
+
 
     fn parse_declaration(&mut self) -> Option<ASTNode> {
         let data_type = self.advance().clone();
@@ -289,7 +293,9 @@ impl Parser
             }
             _ => panic!("Expected value for declaration."),
         };
-        self.symbol_table.insert(identifier.clone(), data_type.clone());
+        self.symbol_table
+            .insert(identifier.clone(), data_type.clone());
+        self.value_table.insert(identifier.clone(), value.clone());
         Some(ASTNode::Declaration {
             data_type,
             identifier: Token::Identifier(identifier),
@@ -297,167 +303,47 @@ impl Parser
         })
     }
 
-    fn parse_print(&mut self) -> Option<ASTNode>
-    {
+    fn parse_print(&mut self) -> Option<ASTNode> {
         self.consume(&Token::Print);
         self.consume(&Token::LParen);
         let print_stmt = match self.peek() {
-            Token::StringLiteral(text) =>
-            {
+            Token::StringLiteral(text) => {
                 let print_stmt = ASTNode::Print(Token::StringLiteral(text.clone()));
                 print_stmt
             }
-            Token::Number(num) =>
-            {
+            Token::Number(num) => {
                 let print_stmt = ASTNode::Print(Token::Number(num.parse().unwrap()));
                 print_stmt
             }
-            Token::Identifier(text) =>
-            {
+            Token::Identifier(text) => {
                 let print_stmt = ASTNode::Print(Identifier(text.clone()));
                 print_stmt
             }
-            _ => { panic!("Expected string literal"); }
+            _ => {
+                panic!("Expected string literal");
+            }
         };
         self.advance();
         self.consume(&Token::RParen);
         Some(print_stmt)
     }
 
-    fn parse_if_condition1(&mut self) -> Option<ASTNode>
-    {
-        let mut if_ast = Vec::new();
-        self.consume(&Token::If);
-        let logic_expression = self.parse_logic_expression();
-        if_ast.push(logic_expression.unwrap());
-        self.consume(&Token::LBrace);
-        while !self.check(&Token::RBrace) {
-            match self.peek() {
-                Token::Print => {
-                    if let Some(print_stmt) = self.parse_print() {
-                        if_ast.push(print_stmt);
-                    }
-                },
-                Token::IntegerType | Token::StringType | Token::BooleanType => {
-                    if let Some(declaration) = self.parse_declaration() {
-                        if_ast.push(declaration);
-                    }
-                },
-                Token::If => {
-                    if let Some(if_stmt) = self.parse_if_condition(){
-                        if_ast.push(if_stmt);
-                    }
-                }
-                Token::ElseIf => {
-                    if let Some(else_if_stmt) = self.parse_else_if_condition(){
-                        if_ast.push(else_if_stmt);
-                    }
-                }
-                Token::Else => {
-                    if let Some(else_stmt ) = self.parse_else_condition(){
-                        if_ast.push(else_stmt);
-                    }
-                }
-                _ => {
-                    panic!("Unsupported feature.")
-                }
-            }
+    fn match_number(&mut self) -> Option<Token> {
+        if let Token::Number(_) = self.peek() {
+            Some(self.advance().clone())
+        } else {
+            None
         }
-        self.consume(&Token::RBrace);
-        Some(ASTNode::If{
-            block: if_ast
-        })
     }
 
-    fn parse_else_if_condition2(&mut self) -> Option<ASTNode>
-    {
-        let mut ast = Vec::new();
-        self.consume(&Token::ElseIf);
-        let logic_expression = self.parse_logic_expression();
-        ast.push(logic_expression.unwrap());
-        self.consume(&Token::LBrace);
-        while !self.check(&Token::RBrace) {
-            match self.peek() {
-                Token::Print => {
-                    if let Some(print_stmt) = self.parse_print() {
-                        ast.push(print_stmt);
-                    }
-                },
-                Token::IntegerType | Token::StringType | Token::BooleanType => {
-                    if let Some(declaration) = self.parse_declaration() {
-                        ast.push(declaration);
-                    }
-                },
-                Token::If => {
-                    if let Some(if_stmt) = self.parse_if_condition(){
-                        ast.push(if_stmt);
-                    }
-                }
-                Token::ElseIf => {
-                    if let Some(else_if_stmt) = self.parse_else_if_condition(){
-                        ast.push(else_if_stmt);
-                    }
-                }
-                Token::Else => {
-                    if let Some(else_stmt ) = self.parse_else_condition(){
-                        ast.push(else_stmt);
-                    }
-                }
-                _ => {
-                    panic!("Unsupported feature.")
-                }
-            }
+    fn match_identifier(&mut self) -> Option<Token> {
+        if let Token::Identifier(_) = self.peek() {
+            Some(self.advance().clone())
+        } else {
+            None
         }
-        self.consume(&Token::RBrace);
-        Some(ASTNode::ElseIf{
-            block: ast
-        })
     }
-
-    fn parse_else_condition2(&mut self) -> Option<ASTNode>
-    {
-        let mut else_ast = Vec::new();
-        self.consume(&Token::Else);
-        self.consume(&Token::LBrace);
-        while !self.check(&Token::RBrace) {
-            match self.peek() {
-                Token::Print => {
-                    if let Some(print_stmt) = self.parse_print() {
-                        else_ast.push(print_stmt);
-                    }
-                },
-                Token::IntegerType | Token::StringType | Token::BooleanType => {
-                    if let Some(declaration) = self.parse_declaration() {
-                        else_ast.push(declaration);
-                    }
-                },
-                Token::If => {
-                    if let Some(if_stmt) = self.parse_if_condition(){
-                        else_ast.push(if_stmt);
-                    }
-                }
-                Token::ElseIf => {
-                    if let Some(else_if_stmt) = self.parse_else_if_condition(){
-                        else_ast.push(else_if_stmt);
-                    }
-                }
-                Token::Else => {
-                    if let Some(else_stmt ) = self.parse_else_condition(){
-                        else_ast.push(else_stmt);
-                    }
-                }
-                _ => {
-                    panic!("Unsupported feature.")
-                }
-            }
-        }
-        self.consume(&Token::RBrace);
-        Some(ASTNode::Else{
-            block: else_ast
-        })
-    }
-    fn match_token(&mut self, types: &[Token]) -> bool
-    {
+    fn match_token(&mut self, types: &[Token]) -> bool {
         for token_type in types {
             if self.check(&token_type) {
                 self.advance();
@@ -467,8 +353,7 @@ impl Parser
         false
     }
 
-    fn check(&self, token_types: &Token) -> bool
-    {
+    fn check(&self, token_types: &Token) -> bool {
         if self.is_at_end() {
             false
         } else {
